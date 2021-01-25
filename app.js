cb1659247147e1a42bc39b7fa4e75cfc43c43e74
Wojciech Skirlo <wojciechskiro@gmail.com>
2021-01-25 16:29:55 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const slider = document.querySelector(".slider input");
+const dragLine = document.querySelector(".slider .drag-line");
+const dragLineTwo = document.querySelector(".slider input::-webkit-slider-thumb");
+const img = document.querySelector("#slider-photo-2");
+const btnMoveLeft = document.querySelector(".drag-line span .box:first-of-type");
+const btnMoveRight = document.querySelector(".drag-line span .box:last-of-type");
+
+slider.addEventListener("input", () => {
+    let sliderVal = slider.value;
+    dragLine.style.left = sliderVal + "%";
+    slider.style.left = sliderVal + "%";
+    img.style.width = sliderVal + "%";
+    document.documentElement.style.setProperty("--positionThumb", sliderVal + "%");
+})
+
+btnMoveLeft.addEventListener("click", () => {
+    let sliderVal = parseInt(slider.value);
+    if (window.innerWidth > 576) {
+        if (sliderVal < 20) {
+            sliderVal = 0;
+        } else {
+            sliderVal -= 20;
+        }
+    } else {
+        if (sliderVal < 25) {
+            sliderVal = 0;
+        } else {
+            sliderVal -= 25;
+        }
+    }
+    slider.value = sliderVal;
+    dragLine.style.left = sliderVal + "%";
+    slider.style.left = sliderVal + "%";
+    img.style.width = sliderVal + "%";
+    document.documentElement.style.setProperty("--positionThumb", sliderVal + "%");
+})
+
+btnMoveRight.addEventListener("click", () => {
+    let sliderVal = parseInt(slider.value);
+    if (window.innerWidth > 576) {
+        if (sliderVal > 80) {
+            sliderVal = 100;
+        } else {
+            sliderVal += 20;
+        }
+    } else {
+        if (sliderVal > 75) {
+            sliderVal = 100;
+        } else {
+            sliderVal += 25;
+        }
+    }
+    slider.value = sliderVal;
+    dragLine.style.left = sliderVal + "%";
+    slider.style.left = sliderVal + "%";
+    img.style.width = sliderVal + "%";
+    document.documentElement.style.setProperty("--positionThumb", sliderVal + "%");
+})
